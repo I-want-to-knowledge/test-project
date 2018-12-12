82abdd3b4027fe839545d9aa282f51da7cd91474
yanzhen <1031881200@qq.com>
2018-12-12 10:33:16 +0800

    添加测试代码

--- src/main/java/com/geo/source/testmain/publictest/DateTimeTest.java
+++ src/main/java/com/geo/source/testmain/publictest/DateTimeTest.java
@@ -8,8 +8,13 @@ import org.apache.commons.lang.time.DateUtils;
 public class DateTimeTest {
 
 	public static void main(String[] args) {
-		System.out.println("month day:" + DateUtils.truncate(new Date(), Calendar.DAY_OF_MONTH));
-		System.out.println("month:" + DateUtils.truncate(new Date(), Calendar.MONTH));
+		
+		System.out.println("day of week:" + Calendar.getInstance().get(Calendar.DAY_OF_WEEK));
+		System.out.println("day of week:" + Calendar.getInstance().get(Calendar.DAY_OF_WEEK_IN_MONTH));
+		System.out.println("day of week:" + Calendar.getInstance().get(Calendar.WEEK_OF_MONTH));
+		
+		// System.out.println("month day:" + DateUtils.truncate(new Date(), Calendar.DAY_OF_MONTH));
+		// System.out.println("month:" + DateUtils.truncate(new Date(), Calendar.MONTH));
 		// System.out.println("week day:" + DateUtils.truncate(new Date(), Calendar.DAY_OF_WEEK));
 		// System.out.println("year day:" + DateUtils.truncate(new Date(), Calendar.DAY_OF_YEAR));
 		// System.out.println("day week month:" + DateUtils.truncate(new Date(), Calendar.DAY_OF_WEEK_IN_MONTH));
