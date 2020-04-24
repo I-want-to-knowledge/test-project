0ce3626c6a473974032f62d2d6d6e4d4d542b3c6
yz <1031881200>
2020-04-24 09:05:47 +0800

    git两个项目合并测试！

---
+++ src/main/java/com/geo/source/git/Test1.java
@@ -0,0 +1,11 @@
+package com.geo.source.git;
+
+/**
+ * @author YanZhen
+ * @date 2020/04/24 09:03
+ **/
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("替换类测试！");
+    }
+}
