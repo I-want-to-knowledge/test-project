23f40cad4585f82476ba9760555d037239c7f960
yz <1031881200>
2020-07-23 17:25:55 +0800

    延迟任务代码优化！

--- src/main/java/com/geo/source/redis/delaytask_v2/DelayTaskProducer.java
+++ src/main/java/com/geo/source/redis/delaytask_v2/DelayTaskProducer.java
@@ -4,15 +4,18 @@ import com.alibaba.fastjson.JSONObject;
 import com.geo.source.redis.delaytask_v2.timer.DelayTaskConstant;
 import com.geo.source.redis.delaytask_v2.timer.DelayTaskDto;
 import com.geo.source.redis.delaytask_v2.timer.DelayTaskTimer;
+import org.springframework.context.ApplicationContext;
 import redis.clients.jedis.JedisCluster;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Map;
 
 /**
  * 延迟任务-生产者
- * 在使用的中心，把该类放进spring的容器内，就可以生产延迟任务了
+ * <pre>
+ * 消费任务：实现DelayTaskConsumer接口，并注入spring容器中（例：使用@Service）；
+ * 如何生产任务：通过注入该类，使用producer方法生产延迟任务
+ * </pre>
  *
  * @author YanZhen
  * @date 2020/04/16 00:19
@@ -29,6 +32,11 @@ public class DelayTaskProducer {
     private String redisKey;
 
     /**
+     * 定时器对象
+     */
+    private DelayTaskTimer timer;
+
+    /**
      * 不允许无参初始化
      */
     private DelayTaskProducer() {
@@ -37,10 +45,11 @@ public class DelayTaskProducer {
     /**
      * 延迟任务生产者
      *
+     * @param context  spring上下文
      * @param redis    redis 初始化
      * @param redisKey 不同中心指定不同的key值，否则会不消费
      */
-    public DelayTaskProducer(JedisCluster redis, String redisKey) {
+    public DelayTaskProducer(ApplicationContext context, JedisCluster redis, String redisKey) {
         if (redisKey == null || redisKey.isEmpty()) {
             throw new IllegalArgumentException("初始化延迟任务生产者，redis key必须输入！");
         }
@@ -49,22 +58,40 @@ public class DelayTaskProducer {
         this.redis = redis;
         this.redisKey = redisKey;
         // 延迟任务计时器启动
-        new DelayTaskTimer(redis, redisKey).afterPropertiesSet();
+        this.timer = new DelayTaskTimer(context, redis, redisKey);
     }
 
     /**
      * 生产任务
      * 注意：通过是实现DelayTaskConsumer接口，进行消费
      *
-     * @param clazz  消费者类
+     * @param clazz  自定义的消费者类实现类
      * @param params 传参
      * @param time   任务延迟消费的时间（单位：秒）
      * @return 是否生产
      */
-    public boolean producer(Class<? extends DelayTaskConsumer> clazz, Map<String, Object> params, long time) {
+    public boolean producer(Class<? extends DelayTaskConsumer> clazz, String params, long time) {
         // 添加消息队列
         final Long aLong = redis.zadd(redisKey, LocalDateTime.now().plusSeconds(time).toEpochSecond(ZoneOffset.of("+8")),
                 JSONObject.toJSONString(new DelayTaskDto(clazz, params)));
-        return aLong != null && aLong > 0;
+
+        boolean b = aLong != null && aLong > 0;
+        if (b) {
+            // 解除定时器的睡眠
+            timer.notifyAll1();
+        }
+        return b;
+    }
+
+    /**
+     * 取消延迟任务
+     * @param clazz 自定义的消费者实现类
+     * @param params 传参；注意：一定要和生成任务时传递的参数相同，否则无法定位redis里的任务
+     * @return 是否成功
+     */
+    public boolean cancel(Class<? extends DelayTaskConsumer> clazz, String params) {
+        // 移除任务，移除成功后消费任务，否则不消费
+        final Long count = redis.zrem(redisKey, JSONObject.toJSONString(new DelayTaskDto(clazz, params)));
+        return count != null && count > 0;
     }
 }
--- src/main/java/com/geo/source/redis/delaytask_v2/timer/DelayTaskTimer.java
+++ src/main/java/com/geo/source/redis/delaytask_v2/timer/DelayTaskTimer.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.geo.source.redis.delaytask_v2.DelayTaskConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import redis.clients.jedis.JedisCluster;
 
 import java.time.LocalDateTime;
@@ -20,7 +21,11 @@ import java.util.concurrent.TimeUnit;
  **/
 public class DelayTaskTimer {
     private Logger logger = LoggerFactory.getLogger(DelayTaskTimer.class);
-    private ScheduledThreadPoolExecutor poolExecutor = new ScheduledThreadPoolExecutor(1, r -> {
+
+    /**
+     * 延迟任务线程池
+     */
+    private ScheduledThreadPoolExecutor poolExecutor = new ScheduledThreadPoolExecutor(8, r -> {
         Thread t = new Thread(r, "Delay_Task_Timer-thread-" + r.hashCode());
         logger.info("{} has been created", t.getName());
         return t;
@@ -37,25 +42,38 @@ public class DelayTaskTimer {
     private String redisKey;
 
     /**
+     * spring上下文
+     */
+    private ApplicationContext context;
+
+    /**
+     * 异常时，默认不终止计时器
+     */
+    private boolean stopTimer = false;
+
+    /**
      * 初始化延迟任务计时器
      *
+     * @param context           spring上下文
      * @param jedisClusterCache redis
      * @param redisKey          key
      */
-    public DelayTaskTimer(JedisCluster jedisClusterCache, String redisKey) {
+    public DelayTaskTimer(ApplicationContext context, JedisCluster jedisClusterCache, String redisKey) {
+        this.context = context;
         this.jedisClusterCache = jedisClusterCache;
         this.redisKey = redisKey;
+
+        afterPropertiesSet();
     }
 
     /**
      * 启动延迟任务计时器
      */
-    public void afterPropertiesSet() {
-        // 核心线程5分钟后无任务终止
+    private void afterPropertiesSet() {
+        // 等待5分钟，无任务线程终止
         poolExecutor.setKeepAliveTime(5, TimeUnit.MINUTES);
         poolExecutor.allowCoreThreadTimeOut(true);
-        // 5秒后开始执行，间隔1秒执行一次
-        poolExecutor.scheduleWithFixedDelay(timer(),5, 1, TimeUnit.SECONDS);
+        poolExecutor.scheduleWithFixedDelay(timer(), 10, 1, TimeUnit.SECONDS);
         logger.info("延迟任务计时器已启动");
     }
 
@@ -64,58 +82,148 @@ public class DelayTaskTimer {
      */
     private Runnable timer() {
         return () -> {
-//        while (true) {
             try {
                 // 业务处理
-                logicalProces();
+                process();
 
-                // 正常休眠1秒
-//                Thread.sleep(1000);
+                // 无任务时休息十分钟，或者有任务放入
+                waitSleep();
             } catch (Exception e) {
                 // 异常的情况
                 logger.error("逻辑处理时，延迟任务计时器异常停止！", e);
-                return;
+
+                // 异常时，计时器是否终止
+                if (stopTimer) {
+                    return;
+                }
+                sysSleep();
             }
-//        }
         };
+    }
+
+    /**
+     * 当意外终止，系统休息30分钟
+     */
+    private void sysSleep() {
+        try {
+            Thread.sleep(DelayTaskConstant.SYSTEM_ERROR_SLEEP);
+        } catch (InterruptedException ex) {
+            logger.error("sleep异常！", ex);
+        }
+    }
+
+    /**
+     * 无任务时休息十分钟，或者有任务放入则结束休息
+     *
+     * @throws InterruptedException 非法调用异常
+     */
+    private void waitSleep() throws InterruptedException {
+        Long zCount = jedisClusterCache.zcard(redisKey);
+        if (zCount == null || zCount < 1) {
+            synchronized (this) {
+                this.wait(DelayTaskConstant.SYSTEM_NOTHING_SLEEP);
+            }
+        }
+    }
+
+    /**
+     * 解除睡眠，开始运行
+     */
+    public void notifyAll1() {
+        poolExecutor.execute(() -> {
+            synchronized (this) {
+                this.notifyAll();
+            }
+        });
     }
 
     /**
      * 逻辑处理
      */
-    private void logicalProces() {
+    private void process() {
         // 查询redis队列中有无要执行的任务
         final Set<String> set = jedisClusterCache.zrangeByScore(redisKey, 0, LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8")));
         if (set == null || set.isEmpty()) {
             return;
         }
 
-        // 有多个任务的情况
+        // 有多个任务的情况，循环放入线程池进行处理
         for (String v : set) {
-            poolExecutor.execute(() -> {
-                // 解析入参和目标消费者
-                final DelayTaskDto dto;
-                final DelayTaskConsumer consumer;
-                try {
-                    dto = JSONObject.parseObject(v, DelayTaskDto.class);
-                    consumer = dto.getClazz().newInstance();
-                } catch (Exception e) {
-                    logger.error("延迟定时器查寻redis的结果（{}）解析失败，可能原因：类未找到！", v, e);
-                    return;
-                }
-                // 移除任务
-                final Long zrem = jedisClusterCache.zrem(redisKey, v);
-                if (zrem != null && zrem > 0) {
-                    try {
-                        // 消费任务
-                        consumer.consumer(dto.getParams());
-                    } catch (Exception e) {
-                        logger.error("参数为（{}），初始化消费者时出现异常，延迟任务执行失败！", v, e);
-                    }
-                } else {
-                    logger.error("参数为（{}），Redis中该值已经不存在，重复删除！", v);
-                }
-            });
+            poolExecutor.execute(() -> processTask(v));
         }
     }
+
+    /**
+     * 处理任务
+     *
+     * @param v redis value
+     */
+    private void processTask(String v) {
+        // 解析入参和目标消费者
+        final DelayTaskDto dto;
+        final DelayTaskConsumer consumer;
+        try {
+            // 解析传递的实体
+            dto = JSONObject.parseObject(v, DelayTaskDto.class);
+            // 初始化消费者，需要注入时通过构造函数初始化
+            consumer = getDelayTaskConsumer(dto);
+        } catch (Exception e) {
+            logger.error("延迟定时器查寻redis的结果（{}）解析失败，可能原因：类未找到！", v, e);
+            // 解析失败，从redis中删除
+            jedisClusterCache.zrem(redisKey, v);
+            logger.warn("延迟任务为（key : {}，value : {}）解析失败，该延迟任务删除！", redisKey, v);
+            return;
+        }
+
+        // 移除任务，移除成功后消费任务，否则不消费
+        final Long zrem = jedisClusterCache.zrem(redisKey, v);
+        if (zrem == null || zrem < 1) {
+            logger.error("参数为（{}），Redis中该值已经不存在，重复删除！", v);
+            return;
+        }
+
+        try {
+            // 消费任务
+            consumer.consumer(dto.getParams());
+        } catch (Exception e) {
+            logger.error("参数为（{}），初始化消费者时出现异常，延迟任务执行失败！", v, e);
+        }
+    }
+
+    /**
+     * 初始化消费者，需要注入时通过构造函数初始化
+     *
+     * @param dto 延时任务传递实体
+     * @return 初始化的消费者
+     */
+    private DelayTaskConsumer getDelayTaskConsumer(DelayTaskDto dto) {
+        // 根据消费者实现类名，从spring容器中获取
+        return context.getBean(dto.getClazz());
+    }
+
+    /**
+     * 异常时是否终止计时器，默认不终止
+     *
+     * @param flag 是否终止，true是
+     */
+    public void setFlag(boolean flag) {
+        this.stopTimer = flag;
+    }
+
+//    /**
+//     * 首字母小写
+//     *
+//     * @param string 字符串
+//     * @return 首字母小写
+//     */
+//    private String firstLowerCase(String string) {
+//        if (string == null) {
+//            return null;
+//        }
+//        char[] ch = string.toCharArray();
+//        if (ch[0] >= 'A' && ch[0] <= 'Z') {
+//            ch[0] += 32;
+//        }
+//        return String.valueOf(ch);
+//    }
 }
